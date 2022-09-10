0176bb21f7f9563af7a5b503e70f3aaed5d80466
yamini2877 <113200678+yamini2877@users.noreply.github.com>
2022-09-09 19:01:28 -0500

    Add Constants
    Co-Authored-By: yamini2877 <113200678+yamini2877@users.noreply.github.com>

---
+++ src/main/java/com/charter/user/rewards/constants/Constants.java
@@ -0,0 +1,11 @@
+package com.charter.user.rewards.constants;
+
+public class Constants {
+
+    public static final String GLOBAL_SERVICE_PATH = "/users";
+    public static final String REWARDS_PATH = "/{userId}/rewards";
+    public static final String TRANSACTION_PATH = "/{userId}/transactions";
+
+    public static final String SUCCESS = "Success";
+    public static final int TWO_POINTS = 2;
+}
